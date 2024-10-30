26312757813f977c002f1d032250a2abd2f362fd
YajatKaul <68262974+yajatkaul@users.noreply.github.com>
2024-10-30 14:31:00 +0530

    Initial commit for Fabric version

--- src/main/java/com/yajatkaul/badmeteoroid/BadMeteoroid.java
+++ src/main/java/com/yajatkaul/badmeteoroid/BadMeteoroid.java
@@ -1,8 +1,24 @@
 package com.yajatkaul.badmeteoroid;
 
-import net.neoforged.fml.common.Mod;
+import net.fabricmc.api.ModInitializer;
 
-@Mod(BadMeteoroid.MOD_ID)
-public class BadMeteoroid {
-    public static final String MOD_ID = "badmeteoroid";
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BadMeteoroid implements ModInitializer {
+	public static final String MOD_ID = "badmeteoroid";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
 }
